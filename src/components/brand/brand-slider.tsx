f6e2807979218e3cef80439b2cdc71c8d4606dff
MudasirTech786 <m.mudasirasfi@gmail.com>
2025-11-12 14:17:59 +0500

    changes

--- src/components/brand/brand-slider.tsx
+++ src/components/brand/brand-slider.tsx
@@ -14,7 +14,6 @@ import b_7 from "@/assets/img/home-01/brand/brand-7.png";
 import b_8 from "@/assets/img/home-01/brand/brand-8.png";
 import b_9 from "@/assets/img/home-01/brand/brand-9.png";
 import b_10 from "@/assets/img/home-01/brand/brand-10.png";
-import b_11 from "@/assets/img/home-01/brand/brand-11.png";
 import b_12 from "@/assets/img/home-01/brand/brand-12.png";
 import b_13 from "@/assets/img/home-01/brand/brand-13.png";
 import b_14 from "@/assets/img/home-01/brand/brand-14.png";
@@ -28,13 +27,18 @@ import b_21 from "@/assets/img/home-01/brand/brand-21.png";
 import b_22 from "@/assets/img/home-01/brand/brand-22.png";
 import b_23 from "@/assets/img/home-01/brand/brand-23.png";
 import b_26 from "@/assets/img/home-01/brand/brand-26.png";
-import b_27 from "@/assets/img/home-01/brand/brand-27.jpeg";
-import b_28 from "@/assets/img/home-01/brand/brand-28.jpg";
-import b_29 from "@/assets/img/home-01/brand/brand-29.jpg";
+import b_27 from "@/assets/img/home-01/brand/brand-27.png";
+import b_28 from "@/assets/img/home-01/brand/brand-28.png";
+import b_29 from "@/assets/img/home-01/brand/brand-29.png";
 import b_30 from "@/assets/img/home-01/brand/brand-30.png";
 
 
-const brand_images = [b_26, b_27, b_28, b_29, b_30, b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_10, b_11, b_12, b_13, b_14, b_15, b_16, b_17, b_18, b_19, b_20, b_21, b_22, b_23, b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_10, b_11, b_12, b_13, b_14, b_15, b_16, b_17, b_18, b_19, b_20, b_21, b_22, b_23];
+const brand_images = [b_26, b_27, b_28, b_29, b_30, b_1, b_2, b_3,
+  b_4, b_5, b_6, b_7, b_8, b_9, b_10, b_12, b_13,
+  b_14, b_15, b_16, b_17, b_18, b_19, b_20, b_21,
+  b_22, b_23, b_1, b_2, b_3, b_4, b_5, b_6, b_7,
+  b_8, b_9, b_10, b_12, b_13, b_14, b_15, b_16,
+  b_17, b_18, b_19, b_20, b_21, b_22, b_23];
 
 export default function BrandSlider() {
   return (
@@ -46,24 +50,11 @@ export default function BrandSlider() {
       >
         {brand_images.map((b, i) => (
           <div key={i} className="tp-brand-item" style={{ width: "140px", margin: "0 24px" }}>
-  <Image src={b} alt="" style={{ width: "100%", height: "auto", objectFit: "contain" }} />
-</div>
+            <Image src={b} alt="" style={{ width: "100%", height: "auto", objectFit: "contain" }} />
+          </div>
 
         ))}
       </Marquee>
-      {/* <Swiper
-        {...slider_setting}
-        modules={[Autoplay, FreeMode]}
-        className="brand-wrapper"
-      >
-        {brand_images.map((b, i) => (
-          <SwiperSlide key={i}>
-            <div className="tp-brand-item">
-              <Image src={b} alt="" />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper> */}
     </div>
   );
 }
